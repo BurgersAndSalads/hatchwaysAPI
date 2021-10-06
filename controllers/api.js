4f8a1cafc4a8200325b15524eb8a4389beca4295
yiren wang <wang_yiren37@hotmail.com>
2021-10-06 17:06:11 -0400

    error handling

--- controllers/api.js
+++ controllers/api.js
@@ -3,7 +3,6 @@ module.exports = {
     posts,
 }
 
-
 function ping(req, res) {
     res.status(200).json({
         success:'true',
@@ -11,10 +10,38 @@ function ping(req, res) {
 }
 
 function posts(req, res) {
-    let tags = req.query.tags.split(',');
+    let tags = req.query.tags;
     let sortBy = req.query.sortBy;
     let direction = req.query.direction;
-    const sortCategory = ['id', 'reads', 'likes', 'popularity'];
-    const sortDirection = ['asc', 'desc'];
+    const sortCategory = ['id', 'reads', 'likes', 'popularity', null, undefined];
+    const sortDirection = ['asc', 'desc', null, undefined];
+    console.log([sortBy, direction]);
+    if (tags) {
+        
+        if (!sortCategory.includes(sortBy)) {
+            res.status(400).json(
+                {
+                    error: 'sortBy parameter is invalid'
+                }
+            )
+        }
+        
+        if (!sortDirection.includes(direction)) {
+            res.status(400).json(
+                {
+                    error: 'direction for sorting is invalid'
+                }
+            )
+        }
+
+        
+
+    } else {
+        res.status(400).json(
+            {
+                error: 'Tags parameter is required'
+            }
+        )
+    }
     res.send([tags,sortBy,direction]);
 }
