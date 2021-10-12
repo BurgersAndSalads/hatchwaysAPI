2ab3080983e574bd915ff2ad3fb2be0c4c6e95ab
yiren wang <wang_yiren37@hotmail.com>
2021-10-12 16:24:39 -0400

    node fetch from api

--- controllers/api.js
+++ controllers/api.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch');
+
 module.exports = {
     ping,
     posts,
@@ -17,20 +19,19 @@ function posts(req, res) {
     const sortDirection = ['asc', 'desc', null, undefined];
 
     if (tags) {
-
         if (!sortCategory.includes(sortBy)) {
             res.status(400).json({error: 'sortBy parameter is invalid'})
         } else if (sortBy == (null || undefined)) {sortBy = 'id'}
         
         if (!sortDirection.includes(direction)) {res.status(400).json({error: 'direction for sorting is invalid'})
         } else if (direction == (null || undefined)) {direction = 'asc'}
-
-        res.status(200).json({
-            
-        })
-
+        
+        res.status(200);
+        fetch(`https://api.hatchways.io/assessment/blog/posts?tag=${tags}&sortBy=${sortBy}&direction=${direction}`)
+            .then(data => data.json())
+            .then(json => res.json(json));
+        
     } else {
         res.status(400).json({error: 'Tags parameter is required'})
     }
-    res.send([tags,sortBy,direction]);
 }
