d1b6e3095b322b3390458f5b34adfd47b0b1a1e1
yiren wang <wang_yiren37@hotmail.com>
2021-10-14 12:37:22 -0400

    clean up, naming

--- controllers/api.js
+++ controllers/api.js
@@ -1,4 +1,3 @@
-const { render } = require('ejs');
 const fetch = require('node-fetch');
 const url = 'https://api.hatchways.io/assessment/blog/posts?tag=';
 
@@ -28,50 +27,27 @@ function postsGet(req, res) {
         if (!sortDirection.includes(direction)) {res.status(400).json({error: 'direction for sorting is invalid'})
         } else if (direction == (null || undefined)) {direction = 'asc'}
         
-
-
         async function renderPosts(tagsList) {
             let posts = [];
             
             async function fetchPosts(tag) {
-                console.log(url+`${tag}`)
                 let response = await fetch(url+`${tag}`);
-                console.log('got the data')
                 let post = await response.json();
                 return post;
             }
 
-            async function forTags (tags) {
+            async function forTags(tags) {
                 for (let i=0; tags.length > i; i++) {
                     posts[i] = await fetchPosts(tags[i]);
                 }
-                console.log('out of the loop')
                 return posts;
             }
+ 
             await forTags(tagsList);
-            // tagsList.forEach(tag => {
-            //     let data = await fetchPosts(tag);
-            //     posts.push(data);
-            // });
-            console.log('ready to send');
             res.status(200).json(posts);
-            console.log('sent')
         }
         
         renderPosts(tags);
-        // let i =0;
-        // let data = [];
-        // tags.forEach(tag => {
-        //     fetch(url+`${tag}`)
-        //     .then(response => response.json())
-        //     .then(json => {
-        //         json.posts.forEach(post => {
-        //             data[i] = post;
-        //             i++;
-        //         });
-        //         return data
-        //     });
-        // });
 
     } else {
         res.status(400).json({error: 'Tags parameter is required'})
